e794ee255d2346f2aa37ab966537ba095a2b8343
KazuyaMito <mito@wonderpla.net>
2021-04-10 20:08:34 +0900

    add aggregate command

--- app/entry.py
+++ app/entry.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
+import discord
 import mysql.connector
 from urllib.parse import urlparse
 import os
 
-bot = commands.Bot(command_prefix='%')
+bot = commands.Bot(command_prefix='/')
 url = urlparse(os.environ['DATABASE_URL'])
 conn = mysql.connector.connect(
     host = url.hostname,
@@ -13,19 +14,24 @@ conn = mysql.connector.connect(
     database = url.path[1:]
 )
 
+
 @bot.event
 async def on_ready():
     conn.ping(reconnect=True)
     print(conn.is_connected())
     print('on_ready')
 
+
 @bot.event
 async def on_message(message):
+    if message.author.bot:
+        return
     conn.ping(reconnect=True)
-    cur = conn.cursor(buffered=True)
 
     if message.content.startswith('!use ブースト'):
+        cur = conn.cursor(buffered=True)
         user_id = message.author.id
+        mention_name = message.author.name
         use_count = 0
 
         cur.execute("SELECT user_id, use_count FROM users WHERE user_id = %s", (user_id,))
@@ -35,8 +41,23 @@ async def on_message(message):
         else:
             use_count += 1
 
-        cur.execute("INSERT INTO users (user_id, use_count) VALUES (%s, %s) ON DUPLICATE KEY UPDATE use_count = %s", (user_id, use_count, use_count))
+        cur.execute("INSERT INTO users (user_id, user_name, use_count) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE use_count = %s", (user_id, mention_name, use_count, use_count))
         conn.commit()
+
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def aggregate(ctx):
+    conn.ping(reconnect=True)
+    cur = conn.cursor()
+
+    embed = discord.Embed(title="Aggregate Result", description="The result of counting the number of times the \"use\" command was used in MEE6.", color=0xff0000)
+    cur.execute("SELECT user_name, use_count FROM users")
+    for row in cur:
+        embed.add_field(name=row[0], value=row[1], inline=False)
+
+    await ctx.send(embed=embed)
 
 
 bot.run("")
