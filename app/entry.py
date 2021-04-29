aa2d7eaa3018ffd0d2b38a6888e8b855e8eb1996
KazuyaMito <mito@wonderpla.net>
2021-04-29 21:16:58 +0900

    Changed DB connection library from mysqlconnector to SQLAlchemy

--- app/entry.py
+++ app/entry.py
@@ -1,24 +1,12 @@
 from discord.ext import commands
 import discord
-import mysql.connector
-from urllib.parse import urlparse
 import os
+import control_db
 
 bot = commands.Bot(command_prefix='%')
-url = urlparse(os.environ['DATABASE_URL'])
-conn = mysql.connector.connect(
-    host = url.hostname,
-    port = 3306,
-    user = url.username,
-    password = url.password,
-    database = url.path[1:]
-)
-
 
 @bot.event
 async def on_ready():
-    conn.ping(reconnect=True)
-    print(conn.is_connected())
     print('on_ready')
 
 
@@ -26,47 +14,36 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    conn.ping(reconnect=True)
 
     if message.content.startswith('!use ブースト'):
-        cur = conn.cursor(buffered=True)
         user_id = message.author.id
         mention_name = message.author.name
         use_count = 0
 
-        cur.execute("SELECT user_id, use_count FROM users WHERE user_id = %s", (user_id,))
-        if cur.rowcount >= 1:
-            for row in cur:
-                use_count = row[1] + 1
+        user = control_db.get_user(user_id)
+        if user is None:
+            use_count = 1
+            control_db.add_user(user_id, mention_name, use_count)
         else:
-            use_count += 1
-
-        cur.execute("INSERT INTO users (user_id, user_name, use_count) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE use_count = %s", (user_id, mention_name, use_count, use_count))
-        conn.commit()
+            use_count = user.use_count + 1
+            control_db.update_user_use_count(user, use_count)
 
     await bot.process_commands(message)
 
 
 @bot.command()
 async def aggregate(ctx):
-    conn.ping(reconnect=True)
-    cur = conn.cursor()
-
     embed = discord.Embed(title="Aggregate Result", description="The result of counting the number of times the \"use\" command was used in MEE6.", color=0xff0000)
-    cur.execute("SELECT user_name, use_count FROM users")
-    for row in cur:
-        embed.add_field(name=row[0], value=row[1], inline=False)
+    users = control_db.get_users()
+    for user in users:
+        embed.add_field(name=user.user_name, value=user.use_count, inline=False)
 
     await ctx.send(embed=embed)
 
 
 @bot.command()
 async def clear(ctx):
-    conn.ping(reconnect=True)
-    cur = conn.cursor()
-
-    cur.execute("DELETE FROM users")
-    conn.commit()
+    control_db.delete_all_users()
     embed = discord.Embed(title="Use Count Clear is Completed.")
     await ctx.send(embed=embed)
 
