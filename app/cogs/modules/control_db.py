9d4b2f0c674c54a74c9a7e362adac4f75276d506
KazuyaMito <mito@wonderpla.net>
2021-04-30 23:49:52 +0900

    Close the session every time.

--- app/cogs/modules/control_db.py
+++ app/cogs/modules/control_db.py
@@ -45,47 +45,69 @@ if __name__ == "__main__":
     main()
 
 
-Session = orm.sessionmaker(engine)
-session = Session()
-
-
 def add_user(user_id, user_name, use_count):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     user = User(user_id=user_id, user_name=user_name, use_count=use_count)
 
     session.add(user)
     session.commit()
+    session.close()
 
 
 def get_user(user_id):
-    return session.query(User).filter_by(user_id=user_id).one_or_none()
+    Session = orm.sessionmaker(engine)
+    session = Session()
+    user = session.query(User).filter_by(user_id=user_id).one_or_none()
+    session.close()
+    return user
 
 
 def get_users():
-    return session.query(User).all()
+    Session = orm.sessionmaker(engine)
+    session = Session()
+    users = session.query(User).all()
+    session.close()
+    return users
 
 
 def update_user_use_count(user, use_count):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     user.use_count = use_count
     session.commit()
+    session.close()
 
 
 def delete_all_users():
+    Session = orm.sessionmaker(engine)
+    session = Session()
     session.query(User).delete()
     session.commit()
+    session.close()
 
 
 def get_guild(guild_id):
-    return session.query(Guild).filter_by(id=guild_id).one_or_none()
+    Session = orm.sessionmaker(engine)
+    session = Session()
+    guild = session.query(Guild).filter_by(id=guild_id).one_or_none()
+    session.close()
+    return guild
 
 
 def add_guild(guild_id, name):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     guild = Guild(id=guild_id, name=name)
 
     session.add(guild)
     session.commit()
+    session.close()
 
 
 def add_dictionary(word, read, guild_id):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     dictionary = session.query(Dictionary).filter_by(word=word, guild_id=guild_id).one_or_none()
     print(dictionary)
     if isinstance(dictionary, type(None)):
@@ -93,46 +115,68 @@ def add_dictionary(word, read, guild_id):
 
         session.add(dictionary)
         session.commit()
+        session.close()
     else:
         set_dictionary(read, dictionary)
 
 
 def set_dictionary(read, dictionary):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     dictionary.read = read
     session.commit()
+    session.close()
 
 
 def get_dictionary(word, guild_id):
-    return session.query(Dictionary).filter_by(word=word, guild_id=guild_id).one_or_none()
+    Session = orm.sessionmaker(engine)
+    session = Session()
+    dictionary = session.query(Dictionary).filter_by(word=word, guild_id=guild_id).one_or_none()
+    session.close()
+    return dictionary
 
 
 def get_dictionaries(guild_id):
-    return session.query(Dictionary).filter_by(guild_id=guild_id)
+    Session = orm.sessionmaker(engine)
+    session = Session()
+    dictionaries = session.query(Dictionary).filter_by(guild_id=guild_id)
+    session.close()
+    return dictionaries
 
 
 def delete_dictionary(id, guild_id):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     dictionary = session.query(Dictionary).filter_by(id=id, guild_id=guild_id).one_or_none()
 
     if isinstance(dictionary, type(None)):
+        session.close()
         return None
     else:
         session.delete(dictionary)
         session.commit()
+        session.close()
         return True
 
 
 def set_read_name(read_name, guild_id):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     guild = get_guild(guild_id=guild_id)
 
     guild.is_name_read = read_name
     session.commit()
+    session.close()
 
 
 def set_read_multi_line(read_multi, guild_id):
+    Session = orm.sessionmaker(engine)
+    session = Session()
     guild = get_guild(guild_id=guild_id)
 
     guild.is_multi_line_read = read_multi
     session.commit()
+    session.close()
 
 
 @event.listens_for(Pool, "checkout")
