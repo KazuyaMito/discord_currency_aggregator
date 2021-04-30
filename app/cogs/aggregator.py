f27f43317171938e65f91c29f8afbe64efc7c93b
KazuyaMito <mito@wonderpla.net>
2021-04-30 14:58:01 +0900

    use Cog

---
+++ app/cogs/aggregator.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+from . import control_db
+
+class Aggregator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener(name='on_message')
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content.startswith('!use ブースト'):
+            user_id = message.author.id
+            mention_name = message.author.name
+            use_count = 0
+
+            user = control_db.get_user(user_id)
+            if user is None:
+                use_count = 1
+                control_db.add_user(user_id, mention_name, use_count)
+            else:
+                use_count = user.use_count + 1
+                control_db.update_user_use_count(user, use_count)
+
+    @commands.command()
+    async def aggregate(self, ctx):
+        embed = discord.Embed(title="Aggregate Result", description="The result of counting the number of times the \"use\" command was used in MEE6.", color=0xff0000)
+        users = control_db.get_users()
+        for user in users:
+            embed.add_field(name=user.user_name, value=user.use_count, inline=False)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def clear(self, ctx):
+        control_db.delete_all_users()
+        embed = discord.Embed(title="Use Count Clear is Completed.")
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    return bot.add_cog(Aggregator(bot))
--- app/entry.py
+++ app/entry.py
@@ -1,51 +1,10 @@
 from discord.ext import commands
-import discord
 import os
-import control_db
 
 bot = commands.Bot(command_prefix='%')
 
 @bot.event
 async def on_ready():
-    print('on_ready')
-
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    if message.content.startswith('!use ブースト'):
-        user_id = message.author.id
-        mention_name = message.author.name
-        use_count = 0
-
-        user = control_db.get_user(user_id)
-        if user is None:
-            use_count = 1
-            control_db.add_user(user_id, mention_name, use_count)
-        else:
-            use_count = user.use_count + 1
-            control_db.update_user_use_count(user, use_count)
-
-    await bot.process_commands(message)
-
-
-@bot.command()
-async def aggregate(ctx):
-    embed = discord.Embed(title="Aggregate Result", description="The result of counting the number of times the \"use\" command was used in MEE6.", color=0xff0000)
-    users = control_db.get_users()
-    for user in users:
-        embed.add_field(name=user.user_name, value=user.use_count, inline=False)
-
-    await ctx.send(embed=embed)
-
-
-@bot.command()
-async def clear(ctx):
-    control_db.delete_all_users()
-    embed = discord.Embed(title="Use Count Clear is Completed.")
-    await ctx.send(embed=embed)
-
+    bot.load_extension("cogs.aggregator")
 
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
